be9d22c79b518a1923bbddab05c980bf7afbf7e9
sajjad_rezaei <sajjad.rezaei9090@gmail.com>
2019-08-15 22:10:58 +0430

    Update index.js
    fixed fixed

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ console.log("this a test");
 console.log("this a test");
 console.log("this a test");
 console.log("sajjad0");
-console.log("sajjad1.1");   
+     dd(log("sajjad1."));   
+
+
+
+
+
 
 
 
